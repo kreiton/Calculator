ac4bacfc00c20d99218b032df76a648b26cf8a53
kreiton <lpfan2003@gmail.com>
2017-08-25 14:58:14 +0300

    New package name

---
+++ src/ru/my/User.java
@@ -0,0 +1,17 @@
+package ru.my;
+import javax.swing.JFrame;
+
+public class User {
+
+	
+	public static void main(String[] args)
+	{
+		Reader r = new Reader("Калькулятор");
+		r.setVisible(true);
+		r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		r.setSize(200, 230);
+		r.setResizable(false);
+		r.setLocationRelativeTo(null);
+		
+	}
+}
